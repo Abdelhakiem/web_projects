fbe007786aa197465976a3b3b4015a222aebb801
Abdelhakiem Osama Eita <73353343+Abdelhakiem@users.noreply.github.com>
2024-09-01 17:27:23 +0300

    Update Collection.js

--- Shopping/pages/Collection.js
+++ Shopping/pages/Collection.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{useEffect,useState}  from 'react'
+import Title from '../components/Title';
+import Card from '../components/Card';
+import { products } from '../assets/frontend_assets/assets';
 
 export default function Collection() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+      console.log(products[0]);
+      setData(products);
+
+  }, []);
+
   return (
-    <div>
-      <h1>Collection </h1>
-    </div>
+     <div>
+     <div>
+     <Title text1="ALL" text2="Collection" />
+     </div>
+     <div className='grid sm:grid-cols-5 md:grid-cols-4 lg:grid-cols4 xl:grid-cols5'>
+
+       {data.map((e) => (
+           <Card 
+           name={e.name} 
+           image={e.image} 
+           price={e.price}
+           />
+       ))}
+       </div>
+
+   </div>
   )
 }
